e7913353a27d03c8d0144d2086ea92679572ada6
Emanuel Escudero <escudero.mdz@gmail.com>
2023-12-01 10:49:07 -0300

    Principal Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <body className='page'>
+      <header className='header'>
+        <h1>Technical Test: Movie Search</h1>
+      </header>
+      <main className='main'>
+        <form className='form'>
+          <label htmlFor="">Enter the name of the movie </label>
+          <div className='inputSearch'>
+            <input type="text" placeholder='Avengers, Star Wars, The Matrix ...' />
+            <button type='submit'>Search</button>
+          </div>
+        </form>
+        <section className='listSection'>
+          Render
+        </section>
+      </main>
+      <footer className='footer'>
+        Footer
+      </footer>
+
+    </body>
   )
 }
 
