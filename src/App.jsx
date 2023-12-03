419ccc768637c73c6950d1c0c0325ec4884764ec
Emanuel Escudero <escudero.mdz@gmail.com>
2023-12-03 13:22:18 -0300

    Added JSON files & Render

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
 import './App.css'
+import responseMovies from './mocks/results.json'
+import noResults from './mocks/no-results.json'
 import Footer from './Components/Footer/Footer'
 import Header from './Components/Header/Header'
 
 function App() {
-
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
 
   return (
     <body className='page'>
@@ -18,7 +21,24 @@ function App() {
           </div>
         </form>
         <section className='listSection'>
-          Render
+          {
+            hasMovies ? (
+              <ul>
+                {
+                  movies.map(movie => (
+                    <li key={movie.imdbID}>
+                      <h3>{movie.Title}</h3>
+                      <p>{movie.Year}</p>
+                      <img src={movie.Poster} alt={`Poster of the movie ${movie.Title}`} />
+                    </li>
+                  ))
+                }
+              </ul>
+            ) : (
+              <p>No found movies for this search</p>
+            )
+          }
+
         </section>
       </main>
       <Footer />
