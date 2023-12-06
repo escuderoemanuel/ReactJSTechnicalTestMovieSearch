982d047256a5a6f0781de19b70c075eb63f43420
Emanuel Escudero <escudero.mdz@gmail.com>
2023-12-06 00:13:13 -0300

    Created Movies Services

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,32 +1,17 @@
-import withResults from '../mocks/results.json'
-import noResults from '../mocks/no-results.json'
 import { useState } from 'react'
-import { API_KEY, PREFIX } from '../constants/constants'
+import { searchMovies } from '../services/movies'
 
 
 export function useMovies({ search }) {
 
-  const [responseMovies, setResponseMovies] = useState([])
-  const movies = responseMovies.Search
+  const [movies, setMovies] = useState([])
 
-  const mappedMovies = movies?.map(movie => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster
-  }))
 
-  const getMovies = () => {
-    if (search) {
-      //setResponseMovies(withResults)
-      fetch(`${PREFIX}${API_KEY}&s=${search}`)
-        .then(res => res.json())
-        .then(json => setResponseMovies(json))
-    } else {
-      setResponseMovies(noResults)
-    }
+  const getMovies = async () => {
+    const newMovies = await searchMovies({ search })
+    setMovies(newMovies)
   }
 
-  return { movies: mappedMovies, getMovies }
+  return { movies, getMovies }
 
 }
---
+++ src/services/movies.js
@@ -0,0 +1,23 @@
+import { API_KEY, PREFIX } from '../constants/constants';
+
+export const searchMovies = async ({ search }) => {
+  if (search === '') return null;
+
+  try {
+    const response = await fetch(`${PREFIX}${API_KEY}&s=${search}`);
+    const json = await response.json();
+
+    const movies = json.Search
+
+    return movies?.map(movie => ({
+      id: movie.imdbID,
+      title: movie.Title,
+      year: movie.Year,
+      poster: movie.Poster
+    }))
+
+
+  } catch (error) {
+    throw new Error('Error searching movies')
+  }
+}
